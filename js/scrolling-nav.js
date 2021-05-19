eb26776152feb3bd3f09bc75bb5ff6dcc4703824
Leon <kldotwon9@gmail.com>
2021-05-19 21:07:46 +0800

    smooth scroll clash

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,6 +1,6 @@
 (function($) {
   "use strict"; // Start of use strict
-
+  /*
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -14,6 +14,7 @@
       }
     }
   });
+  */
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
