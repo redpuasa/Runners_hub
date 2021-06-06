b3040449ebadff9492ade7aae437e971e806b563
Hayati Salleh <hayati.salleh275@gmail.com>
2021-06-06 22:56:12 +0800

    Added User Schema for Private Order

---
+++ models/private_order.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const PrivateOrderSchema = new mongoose.Schema({
+     Username:{
+        type: String,
+        unique: true,
+        lowercase: true,
+        required: true,
+        trim: true
+    },
+    Deli_date:{
+        type: Date,
+        default: Date.now
+    },
+    Deli_time:{
+        //type: String,
+        type: Date,
+        default: Date.now
+    },
+    Pickup_address:{
+        type: String,
+        lowercase: true,
+        required: true,
+        trim: true
+    },
+    Delivery_address:{
+        type: String,
+        lowercase: true,
+        required: true,
+        trim: true
+    },
+    Item_stat:{
+        type: String,
+        possibleValues: ['Paid','Not Paid(Paid by Runner)','Not Paid(Paid Later)']
+    },
+    Phone:{
+        type: Number,
+        required: true,
+        trim: true
+    },
+    Message:{
+        type: String,
+        required: true
+    }
+});
+
+const Private_Order = mongoose.model("Private_Order", PrivateOrderSchema);
+//export the schema
+module.exports = Private_Order;
